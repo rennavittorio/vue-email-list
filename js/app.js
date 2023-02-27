5d5ecc9c0d644a3fd8506633dd8eb2521beecae9
rennavittorio <renna.vittorio@gmail.com>
2023-02-27 15:44:34 +0100

    MLS 1 completed - mail list generated

--- js/app.js
+++ js/app.js
@@ -2,23 +2,46 @@
 
 const { createApp } = Vue
 
-axios
-    .get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then((res) => {
-        console.log(res)
-        //risposta del server
-        console.log(res.data.response)
-        //res.data dati contenuti nella risposta
-    })
+// axios
+//     .get('https://flynn.boolean.careers/exercises/api/random/mail')
+//     .then((res) => {
+//         console.log(res)
+//         //risposta del server
+//         console.log(res.data.response)
+//         //res.data dati contenuti nella risposta
+//     })
 
 createApp({
 
-  data() {
+    data() {
 
-    return {
-      fisrtName: 'marco',
-    }
+        return {
+            firstName: 'marco',
+            mailList: [],
 
-  }
+        }
+
+    },
+
+    methods: {
+        fetchMail(){
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((res) => {
+                    this.mailList.push(res.data.response);
+                })
+                
+        }
+
+    },
+
+    mounted(){
+        for (let i = 0; i < 10; i++){
+            this.fetchMail();
+        }
+
+    },
+
+
 
 }).mount('#app')
